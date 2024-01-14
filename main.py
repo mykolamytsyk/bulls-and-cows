12d86ca664a67c3bffc8fd4a5b8d91e5b5fab3ba
mykolamytsyk <138970828+mykolamytsyk@users.noreply.github.com>
2024-01-14 15:28:15 +0200

    rr

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+from pygame.constants import QUIT
+
+pygame.init()
+
+HEIGHT = 800
+WIDTH = 1200
+
+main_display = pygame.display.set_mode((WIDTH, HEIGHT))
+
+playing = True
+
+while playing:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            playing = False
